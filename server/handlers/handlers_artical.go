d31de985cf2e0c94784f06e3ed57d95ece798133
huanghy <1015850872@qq.com>
2022-06-02 20:10:11 +0800

    fix: fix ztl's stupid mistake

---
+++ server/handlers/handlers_artical.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"nblog-server/server/util/serializer"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Ping 状态检查页面
+func getArti(c *gin.Context) {
+	c.JSON(200, serializer.Response{
+		Code: 0,
+		Msg:  "Pong",
+	})
+}
