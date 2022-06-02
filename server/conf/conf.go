a09dd60f3c3f5073a38d3a532177bd164c9a0179
huanghy <1015850872@qq.com>
2022-06-02 20:10:11 +0800

    fix: fix ztl's stupid mistake

---
+++ server/conf/conf.go
@@ -0,0 +1,17 @@
+package conf
+
+import (
+	"os"
+	"nblog-server/server/util"
+
+	"github.com/joho/godotenv"
+)
+
+// Init 初始化配置项
+func Init() {
+	// 从本地读取环境变量
+	godotenv.Load()
+
+	// 设置日志级别
+	util.BuildLogger(os.Getenv("LOG_LEVEL"))
+}
