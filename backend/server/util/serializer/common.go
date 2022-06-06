00a4109d86d4fc09a0b3514766412e4e9911ce87
iridescentttt <673879891@qq.com>
2022-06-06 14:04:36 +0800

    features:axios cross region

--- backend/server/util/serializer/common.go
+++
@@ -1,9 +0,0 @@
-package serializer
-
-// Response 基础序列化器
-type Response struct {
-	Code  int         `json:"code"`
-	Data  interface{} `json:"data,omitempty"`
-	Msg   string      `json:"msg"`
-	Error string      `json:"error,omitempty"`
-}
