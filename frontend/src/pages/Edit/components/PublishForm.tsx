e19dd195ee1ddcd2bf0e1319f4291260a3dd1b26
iridescentttt <673879891@qq.com>
2022-06-06 22:52:57 +0800

    features: add date

--- frontend/src/pages/Edit/components/PublishForm.tsx
+++ frontend/src/pages/Edit/components/PublishForm.tsx
@@ -6,145 +6,151 @@ import Header from '../type/Header';
 
 type LayoutType = Parameters<typeof Form>[0]['layout'];
 type selfProps = {
-  getHeader: Function;
-  header: Header;
+    getHeader: Function;
+    header: Header;
 };
 
 const App: React.FC<selfProps> = (props) => {
-  const { getHeader, header } = props;
-  const [form] = Form.useForm();
-  const [formLayout, setFormLayout] = useState<LayoutType>('horizontal');
-  const [category, setCategory] = React.useState('');
-  const [tag, setTag] = React.useState(['']);
-  const [title, setTitle] = React.useState('');
-  const [loading, setLoading] = useState(true);
+    const { getHeader, header } = props;
+    const [form] = Form.useForm();
+    const [formLayout, setFormLayout] = useState<LayoutType>('horizontal');
+    const [category, setCategory] = React.useState('');
+    const [tag, setTag] = React.useState(['']);
+    const [title, setTitle] = React.useState('');
+    const [loading, setLoading] = useState(true);
 
-  const onFormLayoutChange = ({ layout }: { layout: LayoutType }) => {
-    setFormLayout(layout);
-  };
+    const onFormLayoutChange = ({ layout }: { layout: LayoutType }) => {
+        setFormLayout(layout);
+    };
 
-  React.useEffect(() => {
-    if (loading && header.title!==undefined) {
-        console.log(header)
-      setTitle(header.title ? header.title : '');
-      setCategory(header.category ? header.category : '');
-      setLoading(false);
-    }
-  }, [header]);
-
-  React.useEffect(() => {
-      if(!loading){
-          getHeader({ title: title, category: category, tag: tag });
-      }
-  }, [title, tag, category]);
-
-  const titleLayout =
-    formLayout === 'horizontal'
-      ? {
-          wrapperCol: { span: 14 },
+    React.useEffect(() => {
+        if (loading && header.title !== undefined) {
+            console.log(header)
+            setTitle(header.title ? header.title : '');
+            setCategory(header.category ? header.category : '');
+            setLoading(false);
         }
-      : null;
-  const categoryLayout =
-    formLayout === 'horizontal'
-      ? {
-          //   labelCol: { span: 1 },
-          wrapperCol: { span: 5 },
-        }
-      : null;
-  const onValuesChange = (_: any, allValues: any) => {
-    const parseValues = allValues['names'];
-    parseValues.forEach((val: string | undefined, key:number) => {
-        if (typeof parseValues[key] === 'undefined') {
-            parseValues[key]=''
-        }
-    });
-    setTag(parseValues);
-  };
+    }, [header]);
 
-  return (
-    <div>
-      <Form
-        {...titleLayout}
-        layout={formLayout}
-        form={form}
-        initialValues={{ layout: formLayout }}
-        onValuesChange={onFormLayoutChange}
-        labelAlign="left"
-      >
-        <Form.Item label="题目">
-          <Input
-            style={{ borderRadius: 10 }}
-            placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </Form.Item>
-      </Form>
-      <Form
-        {...categoryLayout}
-        form={form}
-        initialValues={{ layout: 'inline' }}
-        onValuesChange={onFormLayoutChange}
-        labelAlign="left"
-      >
-        <Form.Item label="类别">
-          <Input
-            style={{ borderRadius: 10 }}
-            placeholder="Category"
-            onChange={(e) => setCategory(e.target.value)}
-            value={category}
-          />
-        </Form.Item>
-      </Form>
-      <Form
-        layout={'inline'}
-        initialValues={{ layout: 'inline' }}
-        name="dynamic_form_item"
-        onValuesChange={onValuesChange}
-      >
-        <Form.List name="names" initialValue={[""]}>
-          {(fields, { add, remove }, { errors }) => (
-            <>
-              {fields.map((field, index) => (
-                <Form.Item
-                  required={false}
-                  key={index}
-                  label={index === 0 ? '标签' : ''}
-                  wrapperCol={{ span: 19 }}
-                  //   style={{wrap:{false}}}
+    React.useEffect(() => {
+        if (!loading) {
+            getHeader({ title: title, category: category, tag: tag });
+        }
+    }, [title, tag, category]);
+
+    const onValuesChange = (_: any, allValues: any) => {
+        const parseValues = allValues['names'];
+        parseValues.forEach((val: string | undefined, key: number) => {
+            if (typeof parseValues[key] === 'undefined') {
+                parseValues[key] = ''
+            }
+        });
+        setTag(parseValues);
+    };
+
+    return (
+        <div>
+            <Form
+                layout={'horizontal'}
+                form={form}
+                initialValues={{ layout: 'horizontal' }}
+                labelAlign="left"
+                requiredMark={false}
+            >
+                <Form.Item label="题目"
+                    // name="title"
+                    required
+                    rules={[
+                        {
+                            required: true,
+                            whitespace: true,
+                            message: '请输入标签',
+                        },
+                    ]}
                 >
-                  <Form.Item
-                    {...field}
+                    <Input
+                        style={{ width: '80%', borderRadius: 10 }}
+                        placeholder="Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </Form.Item>
+            </Form>
+            <Form
+                form={form}
+                initialValues={{ layout: 'horizontal' }}
+                labelAlign="left"
+                requiredMark={false}
+            >
+                <Form.Item label="类别"
+                    // name="category"
+                    required
                     validateTrigger={['onChange', 'onBlur']}
                     rules={[
-                      {
-                        required: true,
-                        whitespace: true,
-                        message: '请输入标签',
-                      },
+                        {
+                            required: true,
+                            whitespace: true,
+                            message: '请输入标签',
+                        },
                     ]}
-                    noStyle
-                  >
-                    <Input placeholder="Tag" style={{ width: '60%', borderRadius: 10 }} />
-                  </Form.Item>
-                  {fields.length > 1 ? (
-                    <MinusCircleOutlined
-                      className={styles.dynamic_delete_button}
-                      onClick={() => remove(field.name)}
+                >
+                    <Input
+                        style={{ width: '30%', borderRadius: 10 }}
+                        placeholder="Category"
+                        onChange={(e) => setCategory(e.target.value)}
+                        value={category}
                     />
-                  ) : null}
                 </Form.Item>
-              ))}
-              <Form.Item>
-                <Button type="dashed" onClick={() => add()} icon={<PlusOutlined />}></Button>
-                <Form.ErrorList errors={errors} />
-              </Form.Item>
-            </>
-          )}
-        </Form.List>
-      </Form>
-    </div>
-  );
+            </Form>
+            <Form
+                layout={'inline'}
+                initialValues={{ layout: 'inline' }}
+                name="dynamic_form_item"
+                onValuesChange={onValuesChange}
+            >
+                <Form.List name="names" initialValue={[""]}>
+                    {(fields, { add, remove }, { errors }) => (
+                        <>
+                            {fields.map((field, index) => (
+                                <Form.Item
+                                    required={false}
+                                    key={index}
+                                    label={index === 0 ? '标签' : ''}
+                                    wrapperCol={{ span: 19 }}
+                                //   style={{wrap:{false}}}
+                                >
+                                    <Form.Item
+                                        {...field}
+                                        validateTrigger={['onChange', 'onBlur']}
+                                        rules={[
+                                            {
+                                                required: true,
+                                                whitespace: true,
+                                                message: '请输入标签',
+                                            },
+                                        ]}
+                                        noStyle
+                                    >
+                                        <Input placeholder="Tag" style={{ width: '60%', borderRadius: 10 }} />
+                                    </Form.Item>
+                                    {fields.length > 1 ? (
+                                        <MinusCircleOutlined
+                                            className={styles.dynamic_delete_button}
+                                            onClick={() => remove(field.name)}
+                                        />
+                                    ) : null}
+                                </Form.Item>
+                            ))}
+                            <Form.Item>
+                                <Button type="dashed" onClick={() => add()} icon={<PlusOutlined />}></Button>
+                                <Form.ErrorList errors={errors} />
+                            </Form.Item>
+                        </>
+                    )}
+                </Form.List>
+            </Form>
+        </div>
+    );
 };
 
 export default App;
